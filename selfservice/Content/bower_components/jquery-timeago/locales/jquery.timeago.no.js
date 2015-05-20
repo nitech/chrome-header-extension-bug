cee3b5312401c48e9f09432d544bbe5a76e46480
sp <sp@reknes.no>
2015-05-20 09:34:15 +0200

    Initial commit

---
+++ selfservice/Content/bower_components/jquery-timeago/locales/jquery.timeago.no.js
@@ -0,0 +1,20 @@
+// English shortened
+jQuery.timeago.settings.strings = {
+    prefixAgo: null,
+    prefixFromNow: null,
+    suffixAgo: "",
+    suffixFromNow: "",
+    seconds: "1m",
+    minute: "1m",
+    minutes: "%dm",
+    hour: "1t",
+    hours: "%dt",
+    day: "1d",
+    days: "%dd",
+    month: "1må",
+    months: "%dmå",
+    year: "1år",
+    years: "%dår",
+    wordSeparator: " ",
+    numbers: []
+};
